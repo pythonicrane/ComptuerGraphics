6097d1c729bbf771101d513d2009d60583c0566b
pythonicrane <pythonicrane@gmail.com>
2017-12-11 16:59:48 +0800

    添加注释

--- Simple.cpp
+++ Simple.cpp
@@ -6,9 +6,9 @@
 
 void init(void)
 {
-	glClearColor(1.0, 1.0, 1.0, 0.5);
-	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+	glClearColor(1.0, 1.0, 1.0, 0.5);//窗口背景颜色
+	glMatrixMode(GL_PROJECTION);//矩阵模型，接下来做什么操作
+	gluOrtho2D(0.0, 200.0, 0.0, 150.0);//2D正交投影
 }
 
 void lineSegment(void)
@@ -25,13 +25,14 @@ void lineSegment(void)
 
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(50, 100);
-	glutInitWindowSize(400, 300);
-	glutCreateWindow("An Example OpenGL Program");
-	init();
-	glutDisplayFunc(lineSegment);
-	glutMainLoop();
+	/*GLUT初始化*/
+	glutInit(&argc, argv);//第一个调用的函数，
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//显示方式
+	glutInitWindowPosition(50, 100);//显示位置
+	glutInitWindowSize(400, 300);//窗口大小
+	glutCreateWindow("An Example OpenGL Program");//创建窗口并赋值窗口名字
+	init();//初始化
+	glutDisplayFunc(lineSegment);//调用显示函数
+	glutMainLoop();//循环调用
 	return 0;
 }
